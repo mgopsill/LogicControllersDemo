301d3fe80993dd092e8f2dc02e0e177a65acb4d8
Mike Gopsill <mgopsill@ford.com@ford.com>
2018-09-14 12:58:33 +0100

    Initial Commit

---
+++ LogicControllersDemoTests/LogicControllersDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  LogicControllersDemoTests.swift
+//  LogicControllersDemoTests
+//
+//  Created by Mike Gopsill on 14/09/2018.
+//  Copyright © 2018 Mike Gopsill. All rights reserved.
+//
+
+import XCTest
+@testable import LogicControllersDemo
+
+class LogicControllersDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
